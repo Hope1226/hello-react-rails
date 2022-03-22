8e1b4cd0eadeb014124515fa0828043e9cda3e9a
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-03-22 20:33:31 +0500

    Set up backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: 'json'} do
+    namespace :v1 do
+      resources :messages, only: %i[index]
+    end
+  end
 
   # Defines the root path route ("/")
   # root "articles#index"
