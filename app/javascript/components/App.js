951d792b162ccee05deeba6a99629d5421ba52e2
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-03-22 21:09:25 +0500

    Set up redux

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Greeting from "./greeting/Greeting";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import globalStore from "./redux/configureStore";
+import { Provider } from "react-redux";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" exact element={< Greeting /> } />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={globalStore}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={< Greeting /> } />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
