33d43b5eeebb52ea957b1d80cc5f469b482939d2
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-03-22 20:58:07 +0500

    Set up react router

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'statics#index'
   namespace :api, defaults: {format: 'json'} do
     namespace :v1 do
       resources :messages, only: %i[index]
     end
+  end
+
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
   end
 
   # Defines the root path route ("/")
